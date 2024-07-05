d4da1419d3c672b45ed3358cc8bb15827f573b53
MLSMIT <manuel.luis.sanchez@gmail.com>
2024-07-05 18:58:26 +0200

    c

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='sn-libraries',
     version='0.1.0',
-    
+
     # Package discovery and structure
-    packages=find_namespace_packages(where='src'),  
-    package_dir={'': 'src'},  
+    packages=find_namespace_packages(where='src'),
+    package_dir={'': 'src'},
 
     # Dependencies (with version specifiers for stability)
     install_requires=[
@@ -15,7 +15,7 @@ setup(
         'hikerapi',
         'requests',
         'beautifulsoup4~=4.12.3',
-    ], 
+    ],
 
     # Metadata
     description="A set of Python libraries for automating Facebook and Instagram functions.",
@@ -23,7 +23,7 @@ setup(
     long_description_content_type='text/markdown',
     author="Your Name",
     author_email="your.email@example.com",
-    url="https://github.com/MLSMIT/sn-libraries", 
+    url="https://github.com/MLSMIT/sn-libraries",
 
     # Additional details
     license="MIT",
@@ -37,7 +37,7 @@ setup(
     extras_require={
         'dev': ['pytest', 'coverage']
     },
-    entry_points={ 
+    entry_points={
         'console_scripts': [
             'my_sn_tool = sn_libraries.tools:main',  # Define a command-line entry point
         ],
