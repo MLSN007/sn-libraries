7f30239403e03e01755b53d9de4a8a20e8051fc7
MLSN007 <manuel.no.007@gmail.com>
2024-10-09 17:35:42 +0200

    s

--- src/sn-main/initial_setup.py
+++ src/sn-main/initial_setup.py
@@ -1,13 +1,46 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
+"""
+Initial Setup Script for Google OAuth2 Credentials
+
+This script handles the initial setup for Google OAuth2 credentials, which are necessary
+for authenticating and authorizing access to Google APIs.
+
+Purpose:
+- Import required libraries for handling Google OAuth2 credentials and JSON operations.
+- Set up the foundation for creating, storing, and managing OAuth2 credentials.
+
+Usage:
+This script should be run:
+1. During the first-time setup of the application.
+2. When refreshing or updating OAuth2 credentials.
+3. If the existing credentials have expired or become invalid.
+
+Recommendations:
+- Ensure you have the necessary Google Cloud Project set up with OAuth2 configured.
+- Keep this script separate from your main application code for security reasons.
+- Store sensitive information (like client secrets) securely and not in version control.
+- Consider implementing a check in your main application to verify if this setup
+  has been completed before attempting to use Google APIs.
+
+Note: This script only imports the necessary modules. Additional code for credential
+creation and storage should be implemented as needed.
+
+Dependencies:
+- google-auth
+- google-auth-oauthlib
+"""
+
 import json
 import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file']
 
 def setup_credentials(account_id):
     config_path = os.getenv(f'GOOGLE_SHEETS_CONFIG_{account_id.upper()}')
+    print(f'config_path: {config_path}')
     client_secrets_path = os.getenv(f'GOOGLE_CLIENT_SECRETS_{account_id.upper()}')
+    print(f'client_secrets_path: {client_secrets_path}')
 
     if not config_path or not client_secrets_path:
         raise ValueError(f"Environment variables not set for {account_id}")
