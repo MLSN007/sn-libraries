8dcecaef66861d56438d3b48b6785e981d1f2f94
MLSN007 <manuel.no.007@gmail.com>
2024-10-17 12:11:10 +0200

    up go

--- src/sn_main/initial_setup.py
+++ src/sn_main/initial_setup.py
@@ -64,4 +64,5 @@ def setup_credentials(account_id):
 
 if __name__ == "__main__":
     # setup_credentials("JK")
-    setup_credentials('007')
+    # setup_credentials("007")
+    setup_credentials("NOCA")
